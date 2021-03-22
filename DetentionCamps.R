d996943baf9fb0de0f5d082ae421a20ddb544e1b
caro-b <caroline.anton@web.de>
2021-03-22 22:30:29 +0100

    plotting & OSM

--- DetentionCamps.R
+++ DetentionCamps.R
@@ -18,39 +18,78 @@ library(dplyr)
 # ggmap(p)
 
 
-#### camp locations via OSM ####
-install.packages("osmdata")
+#### OSM data ####
+# camp locations & their area
 library(osmdata)
 
-# bounding box for Xinjiang, using polygon shaped output
+# bounding box for the area of Xinjiang, using polygon shaped output
 bb <- getbb("Xinjiang", format_out = "polygon")
 
-# convert bb to overpass query object
-q <- opq(bb)
-
-# features
-# output as simple features object (or R spatial (sp) - osmdata_sp() )
+# download camps as features
+# convert bb to an overpass query object (API)
 camps_osm <- opq(bb) %>%
   add_osm_feature(key = "prison_camp", value = "re-education") %>%
-  osmdata_sf() %>%
-  osmdata_xml(filename = 'reedcamps.osm')
+  # output as simple features object (or R spatial (sp) - osmdata_sp()) - advantage of sf: use geom_sf() for ggplot2
+  osmdata_sf()
 
 # plotting
 library(ggmap)
+library(ggplot2)
+library(RStoolbox)
 
 # background map
-camps_map <- get_map(getbb("Xinjiang"), maptype = "roadmap", source = "osm", color = "bw")
+map <- get_map(getbb("Xinjiang"), source = "osm", color = "color", maptype="satellite")
 
-ggmap(camps_map) +
-  geom_sf(data = camps_osm$osm_points, inherit.aes = FALSE)
-  
-# import data as sf
-camps_sf <- sf::st_read('reedcamps.osm', layer = 'points')
-  
-ggmap(camps_map) +
-  geom_sf(data = camps_sf, inherit.aes = FALSE)
+# naturalearthdata
+library(rnaturalearth)
+raster_10m <- ne_download(scale = 10, type = 'NE1_HR_LC', category = 'raster')
+plot(raster_10m)
 
-# points included in polgyon areas - only need polygons
+ggmap(map) +
+ # geom_raster(raster_10m) +
+  geom_sf(data = camps_osm$osm_polygons, 
+          # indicates that the aesthetic mappings of the spatial object osm_polygons has to be used
+          inherit.aes = FALSE,
+          colour = "red",
+          fill = "red",
+          alpha = 0.5) +
+  labs(x = "", y = "") +
+  coord_sf(xlim = c(75.00, 100.00), ylim = c(36.00, 48.00))
+
+
+# vector data on top of raster data
+# download vector data of china 
+china <- getData("GADM", country = "CHN", level = 1)
+china$NAME_1
+
+# filter on extent of Xianjiang
+xianjiang <- china[china$NAME_1 == "Xinjiang Uygur",]
+
+# crop raster to extent of vector data
+raster_china <- crop(raster_10m, china)
+
+ggplot() +
+  ggRGB(raster_crop, 1, ggLayer=T, geom_raster=T) +
+  geom_sf(data = camps_osm$osm_polygons, fill="red", aes(alpha= 0.6))
+
+ggplot() +
+  # forceCat - forces raster values to be categorical (will be converted to factor if needed).
+  #ggR(raster_china, 1, ggLayer=T, geom_raster=T) +
+  geom_sf(data=camps_osm$osm_polygons, fill="red", aes(alpha= 0.6)) +
+  geom_polygon(data=xianjiang, aes(alpha=0.2, x=long, y=lat))
+
+# make interactive map with leaflet
+
+
+
+#### Camp data ####
+library(tidyverse)
+# https://docs.google.com/spreadsheets/d/e/2PACX-1vR48u6lKYD21gv6mqM-2dV2lL8axuJ3yG5QJr2KNfG6bZNhy2dXDib_ZyFl9QKwvTRP0EBKZPYczwp9/pubhtml#
+# v1 from 24.09.2020
+camps <- read_csv("data/CampDataset_v1.csv")
+
+# join with OSM area data
+
 
 
 #### Sentinel 2 data ####
@@ -58,31 +97,37 @@ ggmap(camps_map) +
 library(raster)
 library(RStoolbox)
 
-# import multi-band raster stack
-sen2016 <- brick("data/S2_L1C_2016_07_28_stack_B2348.tif")
-sen2020 <- brick("data/S2_L2A_2020_07_17_stack_B2348.tif")
-
-# reproject raster data to CRS WGS84
-sen2016_re <- projectRaster(sen2016, crs = crs(dem))
-sen2020_re <- projectRaster(sen2020, crs = crs(dem))
-
-plot(sen2016)
-plot(sen2020)
-
-plotRGB(sen2016, 3, 2, 1, stretch = "lin")
-plotRGB(sen2020, 3, 2, 1, stretch = "lin")
-
-
 # import SRTM DEM
 dem <- raster("data/SRTM_N43E088.tif")
 
 plot(dem)
 
+# import multi-band raster stack
+sen2017 <- brick("data/S2_L2A_2017_03_02_stack_B2348.tif")
+sen2021 <- brick("data/S2_L2A_2021_03_06_stack_B2348.tif")
+
+# reproject raster data to CRS WGS84
+sen2017_re <- projectRaster(sen2017, crs = crs(dem))
+sen2021_re <- projectRaster(sen2021, crs = crs(dem))
+
+# quick plotting
+plot(sen2017_re)
+plot(sen2021_re)
+
+plotRGB(sen2017_re, 3, 2, 1, stretch = "lin")
+plotRGB(sen2021_re, 3, 2, 1, stretch = "lin")
+
+
+# check raster values
+vals <- getValues(sen2021_re)
+hist(vals)
+
+
 # crop dem to same extent as raster data
-dem_crop <- crop(dem, extent(sen2020_re))
+dem_crop <- crop(dem, extent(sen2021_re))
 
 # resample dem to 10m spatial resolution of sentinel data
-dem_10m <- resample(dem_crop, sen2020_re)
+dem_10m <- resample(dem_crop, sen2021_re)
 
 plot(dem_10m)
 
@@ -94,6 +139,7 @@ dem_roughness <- terrain(dem_10m, opt = "roughness")
 plot(dem_slope)
 
 # idea: mask out all high slopes
+
 
 # #### import ALOS DSM
 # # https://www.eorc.jaxa.jp/ALOS/en/aw3d30/data/index.htm
@@ -112,24 +158,24 @@ plot(dem_slope)
 
 
 #### spectral indices ####
-ndvi_2020 <- spectralIndices(sen2020_re, red = 3, nir = 4, indices = "NDVI")
-plot(ndvi_2020)
+ndvi_2021 <- spectralIndices(sen2021_re, red = 3, nir = 4, indices = "NDVI")
+plot(ndvi_2021)
 # idea: mask out all high NDVI values
 
 
 #### classification ####
 # unsupervised classification
-unsup_2020 <- unsuperClass(sen2020_re, nClasses = 4)
-plot(unsup_2020$map)
+unsup_2021 <- unsuperClass(sen2021_re, nClasses = 4)
+plot(unsup_2021$map)
 
-raster_stack <- stack(sen2020_re, ndvi_2020, dem_slope, dem_aspect, dem_roughness)
+raster_stack <- stack(sen2021_re, ndvi_2021, dem_slope, dem_aspect, dem_roughness)
 
 # include additional environmental info into classification
-unsup_2020_stack <- unsuperClass(raster_stack, nClasses = 4)
+unsup_2021_stack <- unsuperClass(raster_stack, nClasses = 4)
 
 par(mfrow = c(2,1))
-plot(unsup_2020$map)
-plot(unsup_2020_stack$map)
+plot(unsup_2021$map)
+plot(unsup_2021_stack$map)
 
 # idea: Threshold Based Raster Classification
 # http://neonscience.github.io/neon-data-institute-2016//R/classify-by-threshold-R/
@@ -175,13 +221,13 @@ con_gif <- list.files(path = "data/gif_images/", pattern = "*.jpg", full.names =
   map(image_read) %>% 
   image_join() %>% 
   #image_crop("500x500") %>%
-  image_annotate("Dabancheng, Ürümqi, Xinjiang, China: March 2017 - October 2018", location = "+10+10", size = 20, color = "white") %>%
+  image_annotate("Dabancheng, Ürümqi, Xinjiang (China): March 2017 - March 2020", location = "+10+10", size = 20, color = "white") %>%
   image_animate(fps=2) %>% 
   image_write("construction.gif")
 
 
 #### statistics ####
-
+# idea: first do statistical analysis on data to see most common features of camps
 
 
 #### time series ####
